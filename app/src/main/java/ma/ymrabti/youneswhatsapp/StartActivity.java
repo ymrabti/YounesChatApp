27bb07ad38112d6f5a3752a007671a270ed972e6
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-02-03 19:03:19 +0100

    mnnbbn

--- app/src/main/java/ma/ymrabti/youneswhatsapp/StartActivity.java
+++ app/src/main/java/ma/ymrabti/youneswhatsapp/StartActivity.java
@@ -6,12 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser !=null){
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);finish();
+        }
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
