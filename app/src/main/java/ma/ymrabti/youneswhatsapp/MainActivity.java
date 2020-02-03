32ec2a08420f0355bcafa75b0b8c5eebeb7dd567
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-02-03 21:48:25 +0100

    mnnbbn

--- app/src/main/java/ma/ymrabti/youneswhatsapp/MainActivity.java
+++ app/src/main/java/ma/ymrabti/youneswhatsapp/MainActivity.java
@@ -1,6 +1,10 @@
 package ma.ymrabti.youneswhatsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,38 +34,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle("");
-        circleImageView = findViewById(R.id.profile_image);
-        username = findViewById(R.id.username);
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (firebaseUser != null) {
-            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
-            reference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    User user = dataSnapshot.getValue(User.class);
-                    assert user != null;
-                    username.setText(user.getUsername());
-                    if (user.getImageURL().equals("default")){
-                        circleImageView.setImageResource(R.mipmap.ic_launcher);
+        try {
+            circleImageView = findViewById(R.id.profile_image);
+            username = findViewById(R.id.username);
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (firebaseUser != null) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        assert user != null;
+                        username.setText(user.getUsername());
+                        if (user.getImageURL().equals("default")){
+                            circleImageView.setImageResource(R.mipmap.ic_launcher);
+                        }
+                        else{
+                            Glide.with(getApplicationContext()).load(user.getImageURL()).into(circleImageView);
+                        }
                     }
-                    else{
-                        Glide.with(getApplicationContext()).load(user.getImageURL()).into(circleImageView);
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
                     }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
+                });
+            }
+        }
+        catch (Exception exc){
+            Log.println(Log.ERROR,"exception",exc.getMessage()+exc.toString());
         }
     }
 
-    /*@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
@@ -69,13 +77,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.logout:
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),StartActivity.class));
-                finish();
-                return true;
+        if (item.getItemId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), StartActivity.class));
+            finish();
+            return true;
         }
         return false;
-    }*/
+    }
 }
